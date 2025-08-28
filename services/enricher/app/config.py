9dfdc09e3362c8e7c50312c062acc5ec51e27d75
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-28 16:10:44 +0300

    edit config

--- services/enricher/app/config.py
+++ services/enricher/app/config.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 blacklist_path = Path(__file__).resolve().parents[3] / 'data' / 'weapon_list.txt'
 
-# blacklist_path = Path(__file__).resolve().parent.parent.parent / 'data' / 'weapon_list.txt'
 prv_topic1 = "preprocessed_tweets_antisemitic"
 prv_topic0 = "preprocessed_tweets_not_antisemitic"
 next_topic1 = "enriched_preprocessed_tweets_antisemitic"
